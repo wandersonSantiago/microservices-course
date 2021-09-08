684f1520bbd30dde67c2ba8b5dca69d675f45ebd
WandersonSantiago <wandersonsantiago86@gmail.com>
2021-09-08 00:22:44 -0300

    Project hr-api-gateway created

---
+++ hr-api-gateway/src/test/java/com/microservice/hrapigateway/HrApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.hrapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- hr-payroll/src/main/java/com/microservice/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/services/PaymentService.java
@@ -1,10 +1,13 @@
 package com.microservice.hrpayroll.services;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.microservice.hrpayroll.feignclients.WorkerFeignClient;
 import com.microservice.hrpayroll.models.Payment;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class PaymentService {
@@ -12,10 +15,15 @@ public class PaymentService {
 	@Autowired
 	private WorkerFeignClient client;
 
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	public Payment getPayment(long workerId, int days) {
 
 		var worker = client.findById(workerId);
 
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+	
+	public Payment getPaymentAlternative(long workerId, int days) {
+		return new Payment("Alternative", BigDecimal.valueOf(500.00), days);
 	}
 }
