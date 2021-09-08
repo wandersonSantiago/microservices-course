e19312e264eef3d965ded9521f2c443908f1c4d4
WandersonSantiago <wandersonsantiago86@gmail.com>
2021-09-07 21:52:24 -0300

    add restTemplate

--- hr-payroll/src/main/java/com/microservice/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/services/PaymentService.java
@@ -1,15 +1,26 @@
 package com.microservice.hrpayroll.services;
 
-import java.math.BigDecimal;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.microservice.hrpayroll.models.Payment;
+import com.microservice.hrpayroll.models.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", BigDecimal.valueOf(200.00), days);
+
+		var worker = restTemplate.getForObject(workerHost + "/workers/" + workerId, Worker.class);
+
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
