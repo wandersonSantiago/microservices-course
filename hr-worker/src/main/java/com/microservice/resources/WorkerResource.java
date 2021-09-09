01c06f4ea8d61a9231fb4ab0cf443fd123006f4a
WandersonSantiago <wandersonsantiago86@gmail.com>
2021-09-09 20:53:05 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/microservice/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/microservice/resources/WorkerResource.java
@@ -3,6 +3,8 @@ package com.microservice.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,16 +16,27 @@ import com.microservice.services.WorkerService;
 
 import lombok.extern.log4j.Log4j2;
 
+
 @Log4j2
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private WorkerService service;
 
 	@Autowired
 	private Environment environment;
+
+	
+	@GetMapping(value="/configs")
+	public void config() {
+		log.info("Config => " + testConfig);
+	}
 	
 	@GetMapping
 	public List<Worker> findAll() {
@@ -33,7 +46,7 @@ public class WorkerResource {
 	@GetMapping("/{id}")
 	public Worker findById(@PathVariable Long id) {
 		log.info("PORT = " + environment.getProperty("local.server.port"));
-		
+
 		return service.findById(id);
 	}
 
